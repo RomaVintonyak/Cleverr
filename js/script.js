fee9c73369e1c3f539ac2e7287036436d4680a78
RomaVintonyak <19roman23@gmail.com>
2021-04-20 11:38:36 +0300

    finished header section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
   "use script";
-  
+  /*header*/
+  var header = $("#header");
+  $(window).on("scroll", function(){
+    var scrollPos = $(this).scrollTop();
+    if(scrollPos >2){
+      header.addClass("header--fixed");
+    }else{
+      header.removeClass("header--fixed");
+    }
+  });
 });
