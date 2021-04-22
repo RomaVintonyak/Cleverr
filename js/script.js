4715d2c39f08b623daab35d4c58c6852ac3f22c4
RomaVintonyak <19roman23@gmail.com>
2021-04-22 15:48:00 +0300

    create script to table

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ jQuery(document).ready(function () {
   "use script";
   /*header*/
   var header = $("#header");
-  $(window).on("scroll", function(){
+  $(window).on("scroll", function () {
     var scrollPos = $(this).scrollTop();
-    if(scrollPos >2){
+    if (scrollPos > 2) {
       header.addClass("header--fixed");
-    }else{
+    } else {
       header.removeClass("header--fixed");
     }
+  });
+  /*teble script*/
+  var count;
+  var summWrite = $("._summ");
+  $("[data-money]").change(function () {
+    count = 0;
+    var table_arr = $("[data-money]");
+    for (var i = 0; i < table_arr.length; ++i) {
+      if ($(table_arr[i]).prop('checked')) {
+        count += parseInt($(table_arr[i]).val(), 10);
+      }
+    }
+    summWrite.html("$" + count);
   });
 });
